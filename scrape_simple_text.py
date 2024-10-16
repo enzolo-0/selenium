20dcc00e132d9ad1d73708f536944f936ae96dd4
antoni <antoni.dev0@gmail.com>
2024-10-16 08:47:08 -0700

    add scrape_simple_text.py, gitignore, requirements.txt

---
+++ scrape_simple_text.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+title = driver.title
+driver.implicitly_wait(.5)
+text_box = driver.find_element(by=By.NAME, value="my-text")
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button");
+text_box.send_keys("Selenium")
+submit_button.click()
+message = driver.find_element(by=By.ID, value="message")
+text = message.text
+print(f"*****RESULTS:\n{text}")
+time.sleep(5)
+driver.quit()
